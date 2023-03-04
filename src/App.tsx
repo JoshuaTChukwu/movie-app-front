994af62e1314a420b16b12e43ff3cb85d3348ef6
Joshua T Chukwu <JoshuaT.Chukwu@gmail.com>
2023-03-04 21:17:20 +0100

    first routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import {BrowserRouter} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <BrowserRouter>
+    <Routes>
+ <Route path='/'  element={<Login/>} ></Route>
+    </Routes>
     </BrowserRouter>
   );
 }
