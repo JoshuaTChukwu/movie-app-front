bfa1bdee2f4921ae1c4814609690d1264f0358a9
Joshua T Chukwu <JoshuaT.Chukwu@gmail.com>
2023-03-09 20:30:06 +0100

    ok

--- src/components/Home/Movielist.tsx
+++ src/components/Home/Movielist.tsx
@@ -1,8 +1,9 @@
 import React, { Component, useEffect } from 'react';
+import { Button, Form } from 'react-bootstrap';
 import Swal from 'sweetalert2';
 import call from '../../endpoints/calls';
 import MovieCall from '../../endpoints/MovieCall';
-import { AllMovieList } from '../../types/MovieType';
+import { AllMovieList, SearchQuery } from '../../types/MovieType';
 import HomeNav from './Home';
 import styles from './Home.module.css';
 
@@ -47,14 +48,31 @@ class MovieList extends Component<MovieListProps, MovieListState>{
     return
 
   }
-  handleClick(search:string){
 
-  }
+   handleInputChange = (event: any) => {
+    this.setState({... this.setState, title : event.target.value})
+  };
+   handleSubmit = async () => {
+    const request : SearchQuery = {
+        Page : this.state.pageNumber,
+        SearchValue : this.state.title
+    }
+    
+    
+  };
   
   render(){
+    
+    
     return(
       <div>
         <HomeNav></HomeNav>
+        <h1>Movie Search</h1>
+
+        <div className={styles.searchContainer}>
+  <input type="text" placeholder="Search..." onChange={(e) => this.handleInputChange(e)}/>
+  <button type="submit"><i className="fas fa-search"></i></button>
+</div>
 
       </div>
     
